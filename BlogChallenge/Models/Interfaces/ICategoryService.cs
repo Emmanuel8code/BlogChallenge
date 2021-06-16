d4edee301b238535fb8e10d56ba24db9ef3991f0
Emmanuel8code <casemmanuel8@gmail.com>
2021-06-16 17:02:32 -0300

    Add Services of Entities

---
+++ BlogChallenge/Models/Interfaces/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using BlogChallenge.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogChallenge.Models.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> GetAllCategories();
+        Task<Category> GetCategoryById(int id);
+        Task<bool> AddCategory(Category category);
+        Task<bool> UpdateCategory(Category category);
+        Task<bool> DeleteCategory(int id);
+    }
+}
---
+++ BlogChallenge/Models/Interfaces/IPostService.cs
@@ -0,0 +1,17 @@
+﻿using BlogChallenge.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogChallenge.Models.Interfaces
+{
+    public interface IPostService
+    {
+        Task<IEnumerable<Post>> GetAllPosts();
+        Task<Post> GetPostById(int id);
+        Task<bool> AddPost(Post post);
+        Task<bool> UpdatePost(Post post);
+        Task<bool> DeletePost(int id);
+    }
+}
