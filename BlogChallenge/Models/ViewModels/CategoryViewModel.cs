c401b3fb47189075daad06496f2d8fa2e496984d
Emmanuel8code <casemmanuel8@gmail.com>
2021-06-16 21:18:04 -0300

    Add ViewModels in Models folder

---
+++ BlogChallenge/Models/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogChallenge.Models.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Complete el nombre de la categoría")]
+        [MaxLength(100, ErrorMessage = "El nombre no debe contener mas de 100 caracteres")]
+        [Display(Name = "Nombre")]
+        public string Title { get; set; }
+    }
+}
---
+++ BlogChallenge/Models/ViewModels/PostViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogChallenge.Models.ViewModels
+{
+    public class PostViewModel
+    {
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Complete el título de la Publicación")]
+        [MaxLength(100, ErrorMessage = "El título no debe contener mas de 100 caracteres")]
+        [Display(Name = "Título")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Complete el título de la Publicación")]
+        [Display(Name = "Contenido")]
+        public string Contain { get; set; }
+
+        [Display(Name = "Imagen")]
+        public FormFile Image { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de creación")]
+        public DateTime CreatedDate { get; set; }
+
+        [Display(Name = "Categoría")]
+        public string Category { get; set; }
+    }
+}
